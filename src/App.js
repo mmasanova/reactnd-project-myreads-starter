5083d402c9ee7a473785be13947c99296ec79957
Martina Masanova <martina.masanova@gmail.com>
2018-09-14 18:51:44 +0100

    feat: add ListBooksContent component

--- src/App.js
+++ src/App.js
@@ -6,19 +6,10 @@ import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-  }
-
   render() {
     return (
       <div className="app">
-        <Route exact path="/search" component={SearchBooks} />
+        <Route path="/search" component={SearchBooks} />
         <Route exact path="/" component={ListBooks} />
       </div>
     )
